49cee9e91ce8247b0978ef7700c6a8b707468aaf
Efrexz <zyzz_448@hotmail.com>
2025-03-29 13:10:31 -0500

    agregando la url correcta para githubPages

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 
-const isProduction = process.env.NODE_ENV === "production";
-
 export default defineConfig({
   plugins: [react(), svgr()],
-  base: isProduction ? "https://Efrexz.github.io/vetAriels/" : "/", // Base solo en producción
+  base: "https://Efrexz.github.io/vetAriels/",
   resolve: {
     alias: {
       "@assets": "/src/assets",
